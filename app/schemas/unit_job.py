b0a42c7db0a3e22139b14fe78fb8fbf0fadd870e
bramjanssen <brm.janssen@gmail.com>
2025-08-21 17:09:19 +0200

    docs: added docs for unit_jobs

--- app/schemas/unit_job.py
+++ app/schemas/unit_job.py
@@ -1,29 +1,49 @@
 from datetime import datetime
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from app.schemas.enum import ProcessingStatusEnum, ProcessTypeEnum
 
 
 class ServiceDetails(BaseModel):
-    endpoint: str
-    application: str
+    endpoint: str = Field(
+        ...,
+        description="URL to the endpoint where the service is hosted. For openEO, this is the openEO backend. For OGC API Processes, this field should include the base URL of the platform API",
+    )
+    application: str = Field(
+        ...,
+        description="Path to the application that needs to be executed. For openEO this is referring to the public URL of the UDP (JSON) to execute. For OGC API Processes, this field should include the URL path pointing to the hosted service.",
+    )
 
 
 class ProcessingJobSummary(BaseModel):
-    id: int
-    title: str
-    label: ProcessTypeEnum
-    status: ProcessingStatusEnum
+    id: int = Field(..., description="Unique identifier of the processing job")
+    title: str = Field(..., description="Title of the job")
+    label: ProcessTypeEnum = Field(
+        ...,
+        description="Label that is representing the type of the service that will be executed",
+    )
+    status: ProcessingStatusEnum = Field(
+        ..., description="Current status of the processing job"
+    )
 
 
 class ProcessingJobDetails(BaseModel):
-    service: ServiceDetails
-    parameters: dict
-    result_link: Optional[str]
-    created: datetime
-    updated: datetime
+    service: ServiceDetails = Field(
+        ..., description="Details of the service to be executed"
+    )
+    parameters: dict = Field(
+        ..., description="JSON representing the parameters for the service execution"
+    )
+    result_link: Optional[str] = Field(
+        ..., description="URL to the results of the processing job"
+    )
+    created: datetime = Field(..., description="Creation time of the processing job")
+    updated: datetime = Field(
+        ...,
+        description="Timestamp representing the last time that the job details were updated",
+    )
 
 
 class ProcessingJob(ProcessingJobSummary, ProcessingJobDetails):
@@ -31,7 +51,14 @@ class ProcessingJob(ProcessingJobSummary, ProcessingJobDetails):
 
 
 class BaseJobRequest(BaseModel):
-    title: str
-    label: ProcessTypeEnum
-    service: ServiceDetails
-    parameters: dict
+    title: str = Field(..., description="Title of the job to execute")
+    label: ProcessTypeEnum = Field(
+        ...,
+        description="Label that is representing the type of the service that will be executed",
+    )
+    service: ServiceDetails = Field(
+        ..., description="Details of the service to be executed"
+    )
+    parameters: dict = Field(
+        ..., description="JSON representing the parameters for the service execution"
+    )
