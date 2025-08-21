0ae1cfe18072ed311078f5ebb0f725c2f189357c
bramjanssen <brm.janssen@gmail.com>
2025-08-21 17:01:34 +0200

    docs: added body examples for post requests

--- app/routers/tiles.py
+++ app/routers/tiles.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, HTTPException, status
+from typing import Annotated
+from fastapi import APIRouter, HTTPException, status, Body
 from geojson_pydantic import GeometryCollection
 from loguru import logger
 
-from app.schemas.tiles import TileRequest
+from app.schemas.tiles import GridTypeEnum, TileRequest
 from app.services.tiles.base import split_polygon_by_grid
 
 
@@ -18,7 +19,35 @@ router = APIRouter()
     "service’s Max AOI capacity), calculate the number of tiles to be"
     "processed by the upscaling service.",
 )
-def split_in_tiles(payload: TileRequest) -> GeometryCollection:
+def split_in_tiles(
+    payload: Annotated[
+        TileRequest,
+        Body(
+            openapi_examples={
+                "20x20 Grid": {
+                    "summary": "Request to split up area in a 20x20km grid",
+                    "description": "An example request of splitting up a given area of interest into a 20 by 20km grid.",
+                    "value": TileRequest(
+                        grid=GridTypeEnum.KM_20,
+                        aoi={
+                            "coordinates": [
+                                [
+                                    [5.131074140132512, 51.352892918832026],
+                                    [4.836037011633863, 51.331277680080774],
+                                    [4.789036228520871, 51.12326419975835],
+                                    [5.164855813583216, 51.11863683854557],
+                                    [5.192048230607185, 51.33847556306924],
+                                    [5.131074140132512, 51.352892918832026],
+                                ]
+                            ],
+                            "type": "Polygon",
+                        },
+                    ),
+                }
+            }
+        ),
+    ],
+) -> GeometryCollection:
     try:
         logger.debug(f"Splitting tiles in a {payload.grid} formation")
         return split_polygon_by_grid(payload.aoi, payload.grid)
