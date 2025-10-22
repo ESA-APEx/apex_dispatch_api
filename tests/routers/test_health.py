68988c34ba03f714b6bc7236f9483a787ab4ac02
bramjanssen <brm.janssen@gmail.com>
2025-10-22 14:28:49 +0200

    test: fixed tests

--- tests/routers/test_health.py
+++ tests/routers/test_health.py
@@ -2,9 +2,20 @@ from unittest.mock import patch
 
 
 @patch("app.routers.health.check_db_status")
-def test_health(mock_db_status, client):
-    db_status = {"status": "error", "message": "Database connection failed"}
+def test_health_ok(mock_db_status, client):
+    db_status = {
+        "status": "ok",
+    }
     mock_db_status.return_value = db_status
     r = client.get("/health")
     assert r.status_code == 200
     assert r.json() == {"status": "ok", "database": db_status}
+
+
+@patch("app.routers.health.check_db_status")
+def test_health_db_error(mock_db_status, client):
+    db_status = {"status": "error", "message": "Database connection failed"}
+    mock_db_status.return_value = db_status
+    r = client.get("/health")
+    assert r.status_code == 503
+    assert r.json() == {"status": "error", "database": db_status}
