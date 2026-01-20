079d98923b0d69c10ef421f9041ef0637b98e21f
stripodi <simone.tripodi@terradue.com>
2026-01-20 12:23:02 +0100

    feat: ogc-api-processes-client initial plug

--- app/platforms/implementations/ogc_api_process.py
+++ app/platforms/implementations/ogc_api_process.py
@@ -6,7 +6,10 @@ from app.schemas.enum import OutputFormatEnum, ProcessTypeEnum, ProcessingStatus
 from app.schemas.parameters import Parameter
 from app.schemas.unit_job import ServiceDetails
 from stac_pydantic import Collection
-
+from ogc_api_client import Configuration
+from ogc_api_client.api.execute_api import ExecuteApi
+from ogc_api_client.api_client_wrapper import ApiClientWrapper
+from ogc_api_client.rest import ApiException
 
 @register_platform(ProcessTypeEnum.OGC_API_PROCESS)
 class OGCAPIProcessPlatform(BaseProcessingPlatform):
@@ -14,6 +17,15 @@ class OGCAPIProcessPlatform(BaseProcessingPlatform):
     OGC API Process processing platform implementation.
     This class handles the execution of processing jobs on the OGC API Process platform.
     """
+
+    def _create_api_client_instance(
+        self,
+        details: ServiceDetails
+    ) -> ApiClientWrapper:
+        configuration: Configuration = Configuration(
+            host = details.endpoint
+        )
+        return ApiClientWrapper(configuration)
 
     async def execute_job(
         self,
@@ -23,6 +35,8 @@ class OGCAPIProcessPlatform(BaseProcessingPlatform):
         parameters: dict,
         format: OutputFormatEnum,
     ) -> str:
+        api_client = self._create_api_client_instance(details)
+
         raise NotImplementedError("OGC API Process job execution not implemented yet.")
 
     async def execute_synchronous_job(
@@ -33,11 +47,15 @@ class OGCAPIProcessPlatform(BaseProcessingPlatform):
         parameters: dict,
         format: OutputFormatEnum,
     ) -> Response:
+        # This is currently not supported
+
         raise NotImplementedError("OGC API Process job execution not implemented yet.")
 
     async def get_job_status(
         self, user_token: str, job_id: str, details: ServiceDetails
     ) -> ProcessingStatusEnum:
+        api_client = self._create_api_client_instance(details)
+
         raise NotImplementedError(
             "OGC API Process job status retrieval not implemented yet."
         )
@@ -45,6 +63,8 @@ class OGCAPIProcessPlatform(BaseProcessingPlatform):
     async def get_job_results(
         self, user_token: str, job_id: str, details: ServiceDetails
     ) -> Collection:
+        api_client = self._create_api_client_instance(details)
+
         raise NotImplementedError(
             "OGC API Process job result retrieval not implemented yet."
         )
@@ -52,6 +72,8 @@ class OGCAPIProcessPlatform(BaseProcessingPlatform):
     async def get_service_parameters(
         self, user_token: str, details: ServiceDetails
     ) -> List[Parameter]:
+        api_client = self._create_api_client_instance(details)
+
         raise NotImplementedError(
             "OGC API Process service parameter retrieval not implemented yet."
         )
