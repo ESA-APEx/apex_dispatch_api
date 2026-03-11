010fb9ec8b9842fba9a3c3ad4770a1565aa46741
floeschau <frank.loeschau@terradue.com>
2026-03-11 13:11:48 +0100

    Format (ogc_api_process.py)

--- app/platforms/implementations/ogc_api_process.py
+++ app/platforms/implementations/ogc_api_process.py
@@ -17,10 +17,12 @@ from ogc_api_client.api_client_wrapper import ApiClientWrapper
 from ogc_api_client.rest import ApiException
 from ogc_api_client.models.status_info import StatusInfo, StatusCode
 
+
 @register_platform(ProcessTypeEnum.OGC_API_PROCESS)
 class OGCAPIProcessPlatform(BaseProcessingPlatform):
-
-    application_path_regex = re.compile(r"(?P<namespace>.+)/processes/(?P<process_id>[^/]+)$")
+    application_path_regex = re.compile(
+        r"(?P<namespace>.+)/processes/(?P<process_id>[^/]+)$"
+    )
 
     """
     OGC API Process processing platform implementation.
@@ -33,7 +35,6 @@ class OGCAPIProcessPlatform(BaseProcessingPlatform):
             return (None, job_id)
         return tuple(parts)
 
-    
     def _create_api_client_instance(
         self,
         endpoint: str,
@@ -41,7 +42,7 @@ class OGCAPIProcessPlatform(BaseProcessingPlatform):
         user_token: str = None,
     ) -> ApiClientWrapper:
         configuration: Configuration = Configuration(
-            host = f"{endpoint}/{namespace}" if namespace else endpoint
+            host=f"{endpoint}/{namespace}" if namespace else endpoint
         )
 
         additional_args = {}
@@ -62,29 +63,30 @@ class OGCAPIProcessPlatform(BaseProcessingPlatform):
         logger.info(f"Executing OGC API job with title={title}")
         # Output format omitted from request
 
-        api_client = self._create_api_client_instance(details.endpoint, details.namespace, user_token)
+        api_client = self._create_api_client_instance(
+            details.endpoint, details.namespace, user_token
+        )
 
         headers = {
             "accept": "*/*",
-            #"Prefer": "respond-async;return=representation",
-            "Content-Type": "application/json"
+            # "Prefer": "respond-async;return=representation",
+            "Content-Type": "application/json",
         }
         if user_token:
             headers["Authorization"] = f"Bearer {user_token}"
 
-        data = {
-            "inputs": {key: value for key, value in parameters.items()}
-        }
-        
-        content = api_client.execute_simple(process_id=details.application, execute=data, _headers=headers)
+        data = {"inputs": {key: value for key, value in parameters.items()}}
+
+        content = api_client.execute_simple(
+            process_id=details.application, execute=data, _headers=headers
+        )
 
         job_id = content.job_id
-        
+
         # Return the namespace along with the job ID if needed
         if details.namespace:
             return f"{details.namespace}:{job_id}"
         return job_id
-
 
     async def execute_synchronous_job(
         self,
@@ -98,7 +100,6 @@ class OGCAPIProcessPlatform(BaseProcessingPlatform):
 
         raise NotImplementedError("OGC API Process job execution not implemented yet.")
 
-    
     def _map_ogcapi_status(self, ogcapi_status: str) -> ProcessingStatusEnum:
         """
         Map the status returned by OGC API to a status known within the API.
@@ -123,20 +124,19 @@ class OGCAPIProcessPlatform(BaseProcessingPlatform):
             logger.warning("Mapping of unknown OGC API status: %r", ogcapi_status)
             return ProcessingStatusEnum.UNKNOWN
 
-
-    
     async def get_job_status(
         self, user_token: str, job_id: str, details: ServiceDetails
     ) -> ProcessingStatusEnum:
         logger.debug(f"Fetching job status for OGC API job with ID {job_id}")
-        
+
         # Job ID is composed of namespace and internal job id
         namespace, internal_job_id = self._split_job_id(job_id)
-        api_client = self._create_api_client_instance(details.endpoint, namespace, user_token)
+        api_client = self._create_api_client_instance(
+            details.endpoint, namespace, user_token
+        )
 
         status_info = api_client.get_status(job_id=internal_job_id)
         return self._map_ogcapi_status(status_info.status)
-
 
     async def get_job_results(
         self, user_token: str, job_id: str, details: ServiceDetails
@@ -145,11 +145,12 @@ class OGCAPIProcessPlatform(BaseProcessingPlatform):
 
         # Job ID is composed of namespace and internal job id
         namespace, internal_job_id = self._split_job_id(job_id)
-        api_client = self._create_api_client_instance(details.endpoint, namespace, user_token)
+        api_client = self._create_api_client_instance(
+            details.endpoint, namespace, user_token
+        )
 
         result = api_client.get_result(job_id=internal_job_id)
         return Collection(result[0])
-
 
     async def get_service_parameters(
         self, user_token: str, details: ServiceDetails
@@ -160,13 +161,28 @@ class OGCAPIProcessPlatform(BaseProcessingPlatform):
             f"Fetching service parameters for OGC API process with ID {details.application}"
         )
 
-        api_client = self._create_api_client_instance(details.endpoint, details.namespace, user_token)
+        api_client = self._create_api_client_instance(
+            details.endpoint, details.namespace, user_token
+        )
         process_description = api_client.get_process_description(details.application)
 
         for input_id, input_details in process_description.inputs.items():
-            input_type = input_id, input_details.model_dump().get("var_schema", {}).get("actual_instance", {}).get("type", "string")
+            input_type = (
+                input_id,
+                input_details.model_dump()
+                .get("var_schema", {})
+                .get("actual_instance", {})
+                .get("type", "string"),
+            )
             if isinstance(input_type, tuple):
-                input_type = next((t for t in input_type if t in ["date-interval", "bounding-box", "boolean"]), "string")
+                input_type = next(
+                    (
+                        t
+                        for t in input_type
+                        if t in ["date-interval", "bounding-box", "boolean"]
+                    ),
+                    "string",
+                )
 
             parameters.append(
                 Parameter(
