4d15b4be6b2b9d36be009a98c75a42c62781d2bb
bramjanssen <brm.janssen@gmail.com>
2026-03-19 09:50:51 +0100

    chore: fixed linting of options parameter

--- app/schemas/parameters.py
+++ app/schemas/parameters.py
@@ -48,7 +48,7 @@ class Parameter(BaseModel):
         description="Default value of the parameter, if any",
         examples=["default_value"],
     )
-    options: list[Any] = Field(
+    options: list[Any] | None = Field(
         None,
         description="List of valid options for the parameter, if applicable",
         examples=[["option1", "option2"]]
